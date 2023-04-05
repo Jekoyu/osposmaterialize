9d68a584d5d69f91692bb90cd9e91603c9a2eb1d
kusniawan <kusniawan.sisteminformasi@yahoo.com>
2023-04-06 00:17:55 +0700

    accounting

--- application/views/partial/general_js.php
+++ application/views/partial/general_js.php
@@ -15,12 +15,28 @@
 		});
 	}
 
-	$(document).on('change click keyup input paste', 'input.auto-currency', function() {
+	/*$(document).on('change click keyup input paste', 'input.auto-currency', function() {
 	    $(this).val(function(index, value) {
 	        var sign = value.charAt(0),
 	        $return = 0;
 	        // $return = value.replace(/(?!\.)\D/g, "").replace(/(?:\..*)\./g, "").replace(/\.(\d\d)\d?$/, '.$1').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	        $return = value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ".");
+	        if (sign == '-')
+	        {
+	            $return = '-'+$return;
+	        }
+
+	        return $return;
+	    });
+	});*/
+
+	$(document).on('change click paste', 'input.auto-currency', function() {
+	    $(this).val(function(index, value) {
+	        var sign = value.charAt(0),
+	        $return = 0;
+
+	        $return = accounting.formatMoney(accounting.unformat(value, ","), "", 2, ".", ",");
+
 	        if (sign == '-')
 	        {
 	            $return = '-'+$return;
